59b04e4bbf79a34d7cf52dce8c2c44a4370a92a0
luis-jmtz <luis.jmtz2002@gmail.com>
2025-10-13 22:31:26 -0500

    prepping for UI

--- Main.py
+++ Main.py
@@ -7,30 +7,41 @@ from abilities import *
 import pprint
 import streamlit as st
 
-bandit = Creature()
+#  load data to get accurate names lists 
+abilities = pd.read_csv(r"Data\Creature_abilities.tsv", sep="\t")
+armor = pd.read_csv(r"Data\Armor.tsv", sep="\t")
+shields = pd.read_csv(r"Data\Shields.tsv", sep="\t")
+melee = pd.read_csv(r"Data\Melee.tsv", sep="\t")
+projectile = pd.read_csv(r"Data\Projectile.tsv", sep="\t")
+firearms = pd.read_csv(r"Data\Firearms.tsv", sep="\t")
+explosives = pd.read_csv(r"Data\Explosives.tsv", sep="\t")
+weapons_df = [melee, projectile,firearms]
 
-bandit.brawn = 3
-bandit.reflex = 1
-bandit.brains = -2
-bandit.mettle = 0
 
-bandit.add_armor(1)
-bandit.add_weapon(2,6)
-bandit.add_weapon(0,9)
 
-bandit.calc_dpr()
-print(f"DPR: {bandit.dpr}")
+# bandit = Creature()
+# bandit.brawn = 3
+# bandit.reflex = 1
+# bandit.brains = -2
+# bandit.mettle = 0
 
-bandit.add_ability(1)
+# bandit.add_armor(1)
+# bandit.add_weapon(2,6)
+# bandit.add_weapon(0,9)
 
-diff = bandit.calculate_difficulty()
+# bandit.calc_dpr()
+# print(f"DPR: {bandit.dpr}")
 
-print(diff)
+# bandit.add_ability(1)
 
-print(bandit.abilties[0].name)
+# diff = bandit.calculate_difficulty()
 
-bandit.add_ability(1)
+# print(diff)
 
-diff = bandit.calculate_difficulty()
+# print(bandit.abilties[0].name)
 
-print(diff)
+# bandit.add_ability(1)
+
+# diff = bandit.calculate_difficulty()
+
+# print(diff)
