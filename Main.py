bdc27498160380b0d77ac8603e17f9cb3a1be830
luis-jmtz <luis.jmtz2002@gmail.com>
2025-10-15 21:48:25 -0500

    Main now has UI for creature creation

--- Main.py
+++ Main.py
@@ -7,7 +7,6 @@ from abilities import *
 import pprint
 import streamlit as st
 
-#  load data to get accurate names lists 
 # Load data to get accurate names lists 
 abilities_df = pd.read_csv(r"Data\Creature_abilities.tsv", sep="\t")
 armor_df = pd.read_csv(r"Data\Armor.tsv", sep="\t")
@@ -51,7 +50,7 @@ with st.form("creature_form"):
     with col1:
         st.subheader("Basic Information")
         creature_name = st.text_input("Creature Name", "Unnamed Creature")
-        level = st.slider("Level", 0, 20, 1)
+        level = st.number_input("Level", 0, 20, 1)
         size = st.selectbox("Size", 
                            ["Small (-1)", "Medium (0)", "Large (1)", "Huge (2)"],
                            index=1)
@@ -59,21 +58,21 @@ with st.form("creature_form"):
         size_value = size_map[size]
         
         st.subheader("Movement")
-        base_speed = st.slider("Base Speed", 0, 20, 6)
+        base_speed = st.number_input("Base Speed", 0, 20, 6)
         can_fly = st.checkbox("Can Fly")
         can_burrow = st.checkbox("Can Burrow")
         
         st.subheader("Action Economy")
-        ap = st.slider("Action Points (AP)", 2, 10, 2)
-        num_attacks = st.slider("Number of Attacks", 1, 5, 1)
-        focus_points = st.slider("Focus Points", 0, 10, 0)
+        ap = st.number_input("AP", 2, 10, 2)
+        num_attacks = st.number_input("Number of Attacks", 1, 5, 1)
+        focus_points = st.number_input("FP", 0, 10, 0)
     
     with col2:
         st.subheader("Attributes")
-        brawn = st.slider("Brawn", -5, 10, 0)
-        reflex = st.slider("Reflex", -5, 10, 0)
-        brains = st.slider("Brains", -5, 10, 0)
-        mettle = st.slider("Mettle", -5, 10, 0)
+        brawn = st.number_input("Brawn", -5, 10, 0)
+        reflex = st.number_input("Reflex", -5, 10, 0)
+        brains = st.number_input("Brains", -5, 10, 0)
+        mettle = st.number_input("Mettle", -5, 10, 0)
         
         st.subheader("Equipment")
         armor_options = get_armor_options()
@@ -84,40 +83,34 @@ with st.form("creature_form"):
         
     # Weapons section
     st.subheader("Weapons")
-    weapon_col1, weapon_col2, weapon_col3 = st.columns(3)
     
     weapons_to_add = []
-    with weapon_col1:
-        st.write("Melee Weapons")
-        melee_options = get_weapon_options(0)
-        selected_melee = st.multiselect("Select Melee Weapons", list(melee_options.keys()))
-        weapons_to_add.extend([(0, melee_options[w]) for w in selected_melee])
     
-    with weapon_col2:
-        st.write("Projectile Weapons")
-        projectile_options = get_weapon_options(1)
-        selected_projectile = st.multiselect("Select Projectile Weapons", list(projectile_options.keys()))
-        weapons_to_add.extend([(1, projectile_options[w]) for w in selected_projectile])
+    st.write("Melee Weapons")
+    melee_options = get_weapon_options(0)
+    selected_melee = st.multiselect("Select Melee Weapons", list(melee_options.keys()))
+    weapons_to_add.extend([(0, melee_options[w]) for w in selected_melee])
     
-    with weapon_col3:
-        st.write("Firearms")
-        firearm_options = get_weapon_options(2)
-        selected_firearms = st.multiselect("Select Firearms", list(firearm_options.keys()))
-        weapons_to_add.extend([(2, firearm_options[w]) for w in selected_firearms])
+    st.write("Projectile Weapons")
+    projectile_options = get_weapon_options(1)
+    selected_projectile = st.multiselect("Select Projectile Weapons", list(projectile_options.keys()))
+    weapons_to_add.extend([(1, projectile_options[w]) for w in selected_projectile])
+    
+    st.write("Firearms")
+    firearm_options = get_weapon_options(2)
+    selected_firearms = st.multiselect("Select Firearms", list(firearm_options.keys()))
+    weapons_to_add.extend([(2, firearm_options[w]) for w in selected_firearms])
     
     # Explosives section
     st.subheader("Explosives")
-    explosive_col1, explosive_col2 = st.columns(2)
     
-    with explosive_col1:
-        explosive_options = get_explosive_options()
-        selected_explosives = st.multiselect("Select Explosives", list(explosive_options.keys()))
+    explosive_options = get_explosive_options()
+    selected_explosives = st.multiselect("Select Explosives", list(explosive_options.keys()))
     
-    with explosive_col2:
-        explosive_counts = {}
-        for explosive in selected_explosives:
-            count = st.number_input(f"Count for {explosive}", 1, 100, 1, key=f"exp_{explosive}")
-            explosive_counts[explosive_options[explosive]] = count
+    explosive_counts = {}
+    for explosive in selected_explosives:
+        count = st.number_input(f"Count for {explosive}", 1, 100, 1, key=f"exp_{explosive}")
+        explosive_counts[explosive_options[explosive]] = count
     
     # Abilities section
     st.subheader("Abilities")
@@ -170,83 +163,151 @@ with st.form("creature_form"):
         creature.calc_dpr()
         difficulty = creature.calculate_difficulty()
         
-        # Display creature summary
-        st.subheader("Creature Summary")
-        summary_col1, summary_col2 = st.columns(2)
+        # Generate formatted markdown output
+        markdown_output = f"## {creature_name}\n\n"
         
-        with summary_col1:
-            st.write(f"Level: {creature.level}")
-            st.write(f"Size: {size}")
-            st.write(f"Speed: {creature.speed} ft")
-            if creature.canFly:
-                st.write("Can Fly: Yes")
-            if creature.canBurrow:
-                st.write("Can Burrow: Yes")
+        # Basic Info - only show non-default values
+        basic_info = []
+        basic_info.append(f"**Level**: {creature.level}")
+        if size != "Medium (0)":
+            basic_info.append(f"**Size**: {size}")
+        if creature.speed != 6:
+            basic_info.append(f"**Speed**: {creature.speed}")
+        if creature.canFly:
+            basic_info.append("**Flight**: Yes")
+        if creature.canBurrow:
+            basic_info.append("**Burrow**: Yes")
+        
+        if basic_info:
+            markdown_output += "### Basic Information\n" + "  \n".join(basic_info) + "\n\n"
+        
+        # Attributes Table
+        markdown_output += "### Attributes\n"
+        markdown_output += "| Brawn | Reflex | Brains | Mettle |\n"
+        markdown_output += "|-------|--------|--------|--------|\n"
+        markdown_output += f"| {creature.brawn} | {creature.reflex} | {creature.brains} | {creature.mettle} |\n\n"
+        
+        # Combat Stats - only show non-zero/non-default values
+        combat_stats = []
+        combat_stats.append(f"**AC**: {creature.ac}")
+        combat_stats.append(f"**Luck (HP)**: {creature.luck}")
+        combat_stats.append(f"**Attack Bonus**: +{creature.attack_bonus}")
+        combat_stats.append(f"**Save DC**: {creature.save_dc}")
+        combat_stats.append(f"**DPR**: {creature.dpr:.2f}")
+        combat_stats.append(f"**Difficulty**: {difficulty:.2f}")
+        
+        if combat_stats:
+            markdown_output += "### Combat Stats\n" + "  \n".join(combat_stats) + "\n\n"
+        
+        # Action Economy - only show non-default values
+        action_economy = []
+        if creature.ap != 2:
+            action_economy.append(f"**AP**: {creature.ap}")
+        if creature.num_attacks != 1:
+            action_economy.append(f"**Number of Attacks**: {creature.num_attacks}")
+        if creature.focus_points > 0:
+            action_economy.append(f"**FP**: {creature.focus_points}")
+        
+        if action_economy:
+            markdown_output += "### Action Economy\n" + "  \n".join(action_economy) + "\n\n"
+        
+        # Equipment Section
+        has_equipment = False
+        
+        # Armor
+        if creature.armor:
+            has_equipment = True
+            markdown_output += "### Armor\n"
+            armor_data = armor_df[armor_df["id"] == creature.armor.armor_id].iloc[0]
+            armor_cols = [col for col in armor_data.index if col not in ['id', 'cost'] and pd.notna(armor_data[col])]
             
-            st.write(f"Action Points: {creature.ap}")
-            st.write(f"Number of Attacks: {creature.num_attacks}")
-            st.write(f"Focus Points: {creature.focus_points}")
+            markdown_output += "| " + " | ".join(armor_cols) + " |\n"
+            markdown_output += "|" + "|".join(["---"] * len(armor_cols)) + "|\n"
+            markdown_output += "| " + " | ".join(str(armor_data[col]) for col in armor_cols) + " |\n\n"
         
-        with summary_col2:
-            st.write(f"AC: {creature.ac}")
-            st.write(f"Luck (HP): {creature.luck}")
-            st.write(f"Attack Bonus: +{creature.attack_bonus}")
-            st.write(f"Save DC: {creature.save_dc}")
-            st.write(f"DPR: {creature.dpr:.2f}")
-            st.write(f"Difficulty: {difficulty:.2f}")
-        
-        # Equipment and Abilities
-        col4, col5 = st.columns(2)
-        
-        with col4:
-            st.subheader("Equipment")
-            if creature.armor:
-                st.write(f"Armor: {creature.armor.name}")
-            if creature.shield:
-                st.write(f"Shield: {creature.shield.name}")
+        # Shield
+        if creature.shield:
+            has_equipment = True
+            markdown_output += "### Shield\n"
+            shield_data = shields_df[shields_df["id"] == creature.shield.shield_id].iloc[0]
+            shield_cols = [col for col in shield_data.index if col not in ['id', 'cost'] and pd.notna(shield_data[col])]
             
-            if creature.weapons:
-                st.write("Weapons:")
-                for weapon in creature.weapons:
-                    st.write(f"- {weapon.name} (Dmg: {weapon.dmg})")
-            
-            if creature.explosives:
-                st.write("Explosives:")
-                for explosive, count in creature.explosives:
-                    st.write(f"- {explosive.name} x{count} (Dmg: {explosive.dmg}, Radius: {explosive.radius})")
+            markdown_output += "| " + " | ".join(shield_cols) + " |\n"
+            markdown_output += "|" + "|".join(["---"] * len(shield_cols)) + "|\n"
+            markdown_output += "| " + " | ".join(str(shield_data[col]) for col in shield_cols) + " |\n\n"
         
-        with col5:
-            st.subheader("Abilities")
-            if creature.abilties:
-                for ability in creature.abilties:
-                    st.write(f"- {ability.name}: {ability.description} (Type: {ability.type}, Points: {ability.points})")
-            else:
-                st.write("No abilities")
-
-
-# bandit = Creature()
-# bandit.brawn = 3
-# bandit.reflex = 1
-# bandit.brains = -2
-# bandit.mettle = 0
-
-# bandit.add_armor(1)
-# bandit.add_weapon(2,6)
-# bandit.add_weapon(0,9)
-
-# bandit.calc_dpr()
-# print(f"DPR: {bandit.dpr}")
-
-# bandit.add_ability(1)
-
-# diff = bandit.calculate_difficulty()
-
-# print(diff)
-
-# print(bandit.abilties[0].name)
-
-# bandit.add_ability(1)
-
-# diff = bandit.calculate_difficulty()
-
-# print(diff)
+        # Weapons
+        if creature.weapons:
+            has_equipment = True
+            markdown_output += "### Weapons\n"
+            
+            # Get all possible columns from all weapon types (excluding cost)
+            all_weapon_cols = set()
+            for weapon in creature.weapons:
+                weapon_df = weapons_df_list[weapon.weapon_type]
+                weapon_data = weapon_df[weapon_df["id"] == weapon.weapon_id].iloc[0]
+                cols = [col for col in weapon_data.index if col not in ['id', 'cost'] and pd.notna(weapon_data[col])]
+                all_weapon_cols.update(cols)
+            
+            # Create header
+            header_cols = ["Name", "Type"] + sorted([col for col in all_weapon_cols if col != 'name'])
+            markdown_output += "| " + " | ".join(header_cols) + " |\n"
+            markdown_output += "|" + "|".join(["---"] * len(header_cols)) + "|\n"
+            
+            # Add each weapon as a row
+            for weapon in creature.weapons:
+                weapon_type_name = ["Melee", "Projectile", "Firearms"][weapon.weapon_type]
+                weapon_df = weapons_df_list[weapon.weapon_type]
+                weapon_data = weapon_df[weapon_df["id"] == weapon.weapon_id].iloc[0]
+                
+                row_data = [weapon.name, weapon_type_name]
+                for col in header_cols[2:]:  # Skip Name and Type columns
+                    if col in weapon_data and pd.notna(weapon_data[col]):
+                        row_data.append(str(weapon_data[col]))
+                    else:
+                        row_data.append("-")
+                
+                markdown_output += "| " + " | ".join(row_data) + " |\n"
+            markdown_output += "\n"
+        
+        # Explosives
+        if creature.explosives:
+            has_equipment = True
+            markdown_output += "### Explosives\n"
+            
+            # Get all possible columns from explosives (excluding cost)
+            all_explosive_cols = set()
+            for explosive, count in creature.explosives:
+                explosive_data = explosives_df[explosives_df["id"] == explosive.explosive_id].iloc[0]
+                cols = [col for col in explosive_data.index if col not in ['id', 'cost'] and pd.notna(explosive_data[col])]
+                all_explosive_cols.update(cols)
+            
+            # Create header
+            header_cols = ["Name", "Count"] + sorted([col for col in all_explosive_cols if col != 'name'])
+            markdown_output += "| " + " | ".join(header_cols) + " |\n"
+            markdown_output += "|" + "|".join(["---"] * len(header_cols)) + "|\n"
+            
+            # Add each explosive as a row
+            for explosive, count in creature.explosives:
+                explosive_data = explosives_df[explosives_df["id"] == explosive.explosive_id].iloc[0]
+                
+                row_data = [explosive.name, str(count)]
+                for col in header_cols[2:]:  # Skip Name and Count columns
+                    if col in explosive_data and pd.notna(explosive_data[col]):
+                        row_data.append(str(explosive_data[col]))
+                    else:
+                        row_data.append("-")
+                
+                markdown_output += "| " + " | ".join(row_data) + " |\n"
+            markdown_output += "\n"
+        
+        # Abilities - only show if there are any
+        if creature.abilties:
+            markdown_output += "### Abilities\n"
+            for ability in creature.abilties:
+                markdown_output += f"**{ability.name}** ({ability.type}, {ability.points} points)\n"
+                markdown_output += f"{ability.description}\n\n"
+        
+        # Display the formatted output in a text box
+        st.subheader("Formatted Creature Stats")
+        st.text_area("Creature Statistics", markdown_output, height=600)
