f86f3ee1ce6f3427ac76054878abe1ab52a66dfa
luis-jmtz <luis.jmtz2002@gmail.com>
2025-11-01 21:46:33 -0500

    traits from genotype traits displaying dynamically

--- Pages/2_Player_Genotypes.py
+++ Pages/2_Player_Genotypes.py
@@ -3,20 +3,17 @@ import pandas as pd
 
 # load data
 size_speed = pd.read_csv(r"Data\Size_Speed_Genotype.tsv", sep="\t")
-geno_traits =  pd.read_csv(r"Data\Genotype_Traits.tsv", sep="\t")
+geno_traits =  pd.read_csv(r"Data\Genotype_Traits.tsv", sep="\t", escapechar='\\')
+
+geno_traits["description"] = geno_traits["description"].str.replace(r'\\n', '\n', regex=True) #ensures new-line characters are read correclty
+
+# Size List: Used for converting numerical size value to text
 
 # defines the tabs that will be presented and used
 tab_labels = []
 for row in size_speed.itertuples():
     tab_labels.append(row.name)
 tab_labels.pop(0)
-
-# intializes a string that the other functions will modify
-display_text = ""
-
-
-def get_geno_values(geno_id):
-    pass
 
 
 tabs = st.tabs(tab_labels)
@@ -26,7 +23,24 @@ tabs = st.tabs(tab_labels)
 for i, tab in enumerate(tabs):
     with tab:
         geno_name = tab_labels[i]
-        st.write(geno_name)
-        curr_geno_id = int(size_speed.loc[size_speed["name"] == geno_name].get("genotype_id")) # where the name column = the geno name, get the respective id and then cast it to an integer
-        st.write(curr_geno_id)
-        get_geno_values(curr_geno_id)
+        st.header(geno_name)
+        # curr_geno_id = int(size_speed.loc[size_speed["name"] == geno_name].get("genotype_id")) # where the name column = the geno name, get the respective id and then cast it to an integer
+        # st.write(curr_geno_id)
+
+        st.write("### Core Traits")
+
+        ss_row = size_speed[size_speed["genotype_id"] == i+1] # size, speed row
+
+        st.write(ss_row)
+        
+
+
+        for index, row in geno_traits.iterrows():
+            if row.isCore == 1 and row.genotype_id == i+1:
+                st.write(row.description)
+
+        st.write("### Secondary Traits")
+
+        for index, row in geno_traits.iterrows():
+            if row.isCore == 0 and row.genotype_id == i+1:
+                st.write(row.description)
